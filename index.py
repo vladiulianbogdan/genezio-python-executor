6bff1de1044fb7b8231d5b3e7e73ed8a18519b6c
Cristi Miloiu <miloiuc4@gmail.com>
2024-10-28 11:21:26 +0200

    Add streaming example

--- index.py
+++ index.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, Response, stream_with_context
+import time
 
 app = Flask(__name__)
 
@@ -11,10 +12,18 @@ def name_route():
     name = request.args.get('name', 'Unknown')
     return f'Hello, {name}!'
 
+@app.route('/stream')
+def stream():
+    def generate():
+        for i in range(5):
+            time.sleep(1)
+            yield f'Chunk {i}\n'
+    return Response(stream_with_context(generate()))
+
 @app.route('/post-data', methods=['POST'])
 def post_data():
     data = request.get_json()
     if not data:
         return {'error': 'No data found in request'}, 400
     print(f"Received data: {data}")
-    return data
+    return data 
